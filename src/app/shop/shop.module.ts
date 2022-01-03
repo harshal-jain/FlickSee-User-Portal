9ae6629553727d206245d3eecf56293f74be8ec1
Harshal Jain <harshaljain.0008@gmail.com>
2022-01-03 13:48:06 +0530

    New Project

---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopRoutingModule } from './shop-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LogoComponent } from './home/logo/logo.component';
+import { CollectionBannerComponent } from './home/collection-banner/collection-banner.component';
+import { ParallaxBannerComponent } from './home/parallax-banner/parallax-banner.component';
+import { ProductSliderComponent } from './home/product-slider/product-slider.component';
+import { ProductTabComponent } from './home/product-tab/product-tab.component';
+import { SliderComponent } from './home/slider/slider.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LogoComponent,
+    CollectionBannerComponent,
+    ParallaxBannerComponent,
+    ProductSliderComponent,
+    ProductTabComponent,
+    SliderComponent
+  ],
+  imports: [
+    CommonModule,
+    ShopRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule
+  ]
+})
+export class ShopModule { }
